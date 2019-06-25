3a4901d34c345c8cf146e296b5cd3048fe426883
ose Rivas <jose.j.rivasgarcia@gmail.com>
2019-06-24 21:52:43 -0500

    changed js build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3462e50e631d470fdb28761d5ef92c75.js"
+  "./precache-manifest.6d4af0b8f43cfa11cfabb12ced2f9044.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
