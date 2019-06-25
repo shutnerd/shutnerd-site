8031f7df0fec8b833872d1bf56e8a542745979a0
ose Rivas <jose.j.rivasgarcia@gmail.com>
2019-06-24 21:38:47 -0500

    added build

---
+++ build/precache-manifest.b267ea38e4391f59df02ad9c4f3711e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "deae6452556089454eb4",
+    "url": "/static/css/main.542f8052.chunk.css"
+  },
+  {
+    "revision": "deae6452556089454eb4",
+    "url": "/static/js/main.deae6452.chunk.js"
+  },
+  {
+    "revision": "aa67d13d6e9f6822edb4",
+    "url": "/static/js/1.aa67d13d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eaac9936e800260513f5e125e7167140",
+    "url": "/index.html"
+  }
+];
