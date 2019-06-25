24a91348f89c85738fea8a61884d09551f0f1962
ose Rivas <jose.j.rivasgarcia@gmail.com>
2019-06-24 22:07:02 -0500

    Updates

---
+++ precache-manifest.2a438c702144275154d8307888cb093a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fe9c97f2add734f6506",
+    "url": "/shutnerd-site/static/css/main.3e7b6466.chunk.css"
+  },
+  {
+    "revision": "2fe9c97f2add734f6506",
+    "url": "/shutnerd-site/static/js/main.2fe9c97f.chunk.js"
+  },
+  {
+    "revision": "aa67d13d6e9f6822edb4",
+    "url": "/shutnerd-site/static/js/1.aa67d13d.chunk.js"
+  },
+  {
+    "revision": "eecab2be342fb8496e76",
+    "url": "/shutnerd-site/static/js/runtime~main.eecab2be.js"
+  },
+  {
+    "revision": "834307b22c941510f6f9d8aba3d55b85",
+    "url": "/shutnerd-site/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6d4af0b8f43cfa11cfabb12ced2f9044.js"
+  "/shutnerd-site/precache-manifest.2a438c702144275154d8307888cb093a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/shutnerd-site/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
