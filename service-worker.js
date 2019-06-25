592a36bbd5e882674f5f11dbe5b29b7a77fd5cba
ose Rivas <jose.j.rivasgarcia@gmail.com>
2019-06-24 22:17:16 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/shutnerd-site/precache-manifest.2a438c702144275154d8307888cb093a.js"
+  "/shutnerd-site/precache-manifest.389065d7c6a287dba57da3aff0c936ea.js"
 );
 
 workbox.clientsClaim();
