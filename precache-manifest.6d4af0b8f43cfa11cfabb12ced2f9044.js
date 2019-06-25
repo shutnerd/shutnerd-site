a660a954de5a02cb5065897b11be15f014a35d61
ose Rivas <jose.j.rivasgarcia@gmail.com>
2019-06-24 21:54:15 -0500

    changes to js files

---
+++ precache-manifest.6d4af0b8f43cfa11cfabb12ced2f9044.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fe9c97f2add734f6506",
+    "url": "./static/css/main.3e7b6466.chunk.css"
+  },
+  {
+    "revision": "2fe9c97f2add734f6506",
+    "url": "./static/js/main.2fe9c97f.chunk.js"
+  },
+  {
+    "revision": "aa67d13d6e9f6822edb4",
+    "url": "./static/js/1.aa67d13d.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ad3ac21c1916b904917a21346e45ea1c",
+    "url": "./index.html"
+  }
+];
