c6405adb672f93aa9ac8396e8c148516ecfe42f7
ose Rivas <jose.j.rivasgarcia@gmail.com>
2019-07-16 19:17:51 -0500

    Updates

---
+++ precache-manifest.b4581fbd8482404e1be8547c2748f234.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b00fb301e36860565abadc8bfc7d5e52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d016453cca853573bdd9",
+    "url": "/static/css/main.5432d417.chunk.css"
+  },
+  {
+    "revision": "8aff5942395478ac24a2",
+    "url": "/static/js/2.11eab940.chunk.js"
+  },
+  {
+    "revision": "d016453cca853573bdd9",
+    "url": "/static/js/main.234f4758.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
